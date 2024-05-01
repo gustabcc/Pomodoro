53ac9e55a83b580faf6e1388d3ec19c8e9a1b53f
gustabcc <gustavo_hmoura@outlook.com>
2024-05-01 13:44:49 -0300

    Layout do projeto criado

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div>App</div>;
-};
-
-export default App;
+import { ThemeProvider } from 'styled-components'
+import { DefaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/globals'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
---
+++ pomodoro/src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../../components/Header'
+import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from './style'
+export function DefaultLayout() {
+  return (
+    <>
+      <LayoutContainer>
+        <Header />
+        <Outlet />
+      </LayoutContainer>
+    </>
+  )
+}
